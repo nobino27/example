4b40ee614b87e191b0015a0d005646d68b5afc65
nobino27 <lalabibi27@gmail.com>
2018-07-03 05:39:51 +0700

    zuul

---
+++ zuul_eureka/VOD/src/main/java/com/stu/zuul_eureka/ZuulEurekaApplication.java
@@ -0,0 +1,18 @@
+package com.stu.zuul_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+
+public class ZuulEurekaApplication {
+    @RequestMapping(value = "/1")
+    public String listVod() {return "Vod 1";}
+    @RequestMapping(value = "/2")
+    public String vod() {return "Vod 2";}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulEurekaApplication.class, args);
+    }
+}
